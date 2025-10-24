d8b558ea72db9669f41188300a9b0723030f6e07
xiyingruyi <88181161+xiyingruyi@users.noreply.github.com>
2025-10-25 03:40:54 +0800

    Update vip_unlock.js

--- vip_unlock.js
+++ vip_unlock.js
@@ -1,44 +1,9 @@
-let body = $response.body;
-try {
-    let obj = JSON.parse(body);
-    let url = $request.url;
+let obj = JSON.parse($response.body);
 
-    // Vivo rule/get.do
-    if (url.includes('/rule/get.do') && obj.data && Array.isArray(obj.data) && obj.data.length > 0) {
-        let userData = obj.data[0];
-        userData.cstatus = "1";
-        userData.pstatus = "y1";
-        let now = Date.now();
-        userData.ctime = now;
-        userData.ptime = now;
-        console.log("Vivo updated: cstatus=1, pstatus=y1");
-    } 
-    // Buding get_user_config
-    else if (url.includes('/get_user_config') && obj.result && obj.result.user_type !== undefined) {
-        let result = obj.result;
-        result.user_type = 2;
-        result.vip_storage = 20480;
-        result.renewal_status = 1;
-        result.subscribe_plan_validity = 36500;
-        result.subscribe_plan_name = "终身会员";
-        let future = Math.floor(Date.now() + (365 * 24 * 60 * 60 * 1000 * 100));
-        result.end_time = future;
-        result.next_pay_time = future;
-        console.log("Buding updated: user_type=2, vip_storage=20480");
-    } 
-    // Buding paid_modules
-    else if (url.includes('/paid_modules') && obj.result && Array.isArray(obj.result)) {
-        obj.result.forEach(item => {
-            item.usage_limit = -1;
-            if (item.vip_storage_limit !== null) {
-                item.storage_limit = item.vip_storage_limit;
-            }
-        });
-        console.log("Buding modules updated: usage_limit=-1");
-    }
-
-    body = JSON.stringify(obj, null, 2);
-} catch (e) {
-    console.log("Script error: " + e);
+if ($request.url.includes('/rule/get.do')) {
+  obj = {"code":200,"data":[{"cstatus":"1","ctime":Date.now(),"mid":"A553","pstatus":"y1","ptime":Date.now(),"reportFrequency":"200000"}],"msg":""};
+} else if ($request.url.includes('/get_user_config')) {
+  obj = {"code":0,"cost_time":"90 ms","msg":"ok","result":{"avatar":"https://www.budingscan.com/ai_painting_no_auth/img?id=7d084d3f-a867-4e81-b39b-bf02e9b3a637.jpg","email":"","end_time":"2013017600","icode":"","invitation_code":"Q6JFC5","nationCode":"86","next_pay_price":null,"next_pay_time":"2013017600","nickname":"夕影血薇","oral":1,"phone":"XXXXXX","renewal_status":1,"subscribe_pay_type":0,"subscribe_plan_name":"终身会员","subscribe_plan_validity":36500,"sync_type":3,"total_storage":288358400,"uid":"c22f7d0ca75711ed8b19b49691e04e00","used_storage":0,"user_type":2,"vip_storage":20480},"sid":"907a9c76-b10c-11f0-bcef-b49691e0d748","ver":"prd"};
 }
-$done({body});
+
+$done({body: JSON.stringify(obj)});
